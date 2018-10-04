c571dc951cc114a39ca7e837d815741f9cee458a
aslabd <aslamabdurrohim@gmail.com>
2018-10-04 14:17:37 +0700

    update docker

--- forex/configuration.js
+++ forex/configuration.js
@@ -5,7 +5,7 @@ module.exports = {
 			user: 'root',			// database user
 			password: '',			// database password 
 			options: {
-				host: '127.0.0.1',	
+				host: 'abdurrohim.id',	
 				dialect: 'mysql',
 				pool: {
 					max: 5,
